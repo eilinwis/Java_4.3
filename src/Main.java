0c6f2c67aee2fee5112916eba2c5c3cc7b5267a6
Roman Kudryavtsev <TTe4kiH98@gmail.com>
2021-03-15 14:55:24 +0300

    Third

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String [] args){
         CreditPaymentService service = new CreditPaymentService();
-        double monthly_payment = service.calculate(36,1_000_000, 9.99);
-        System.out.println(monthly_payment);
+        double OneYeatmonthly_payment = service.calculate(12,1_000_000, 9.99);
+        System.out.println(OneYeatmonthly_payment);
+
+        double TwoYearsmonthly_payment = service.calculate(24,1_000_000, 9.99);
+        System.out.println(TwoYearsmonthly_payment);
+
+        double ThreeYearsmonthly_payment = service.calculate(36,1_000_000, 9.99);
+        System.out.println(ThreeYearsmonthly_payment);
     }
 }
